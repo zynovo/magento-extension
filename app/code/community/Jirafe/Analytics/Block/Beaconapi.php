73189a49a7f63ff9b9fd16301e83cd0bd8f03910
Richard Loerzel <rloerzel@lyonscg.com>
2013-10-16 14:16:10 -0500

    beacon api changes

--- app/code/community/Jirafe/Analytics/Block/Beaconapi.php
+++ app/code/community/Jirafe/Analytics/Block/Beaconapi.php
@@ -16,13 +16,48 @@ class Jirafe_Analytics_Block_Beaconapi extends Mage_Core_Block_Template
     protected $_beaconApiUrl = null;
     protected $_siteId = null;
     protected $_orgId = null;
-    protected $_page = null;
-    
     
     public function __construct()
     {
         $this->_beaconApiUrl = Mage::getStoreConfig('jirafe_analytics/account/beacon_api') ;
         $this->_siteId = Mage::getStoreConfig('jirafe_analytics/account/site_id') ;
         $this->_orgId = Mage::getStoreConfig('jirafe_analytics/account/org_id');
+        
+    }
+    
+    protected function _getPage()
+    {
+        $request = $this->getRequest();
+        $module = $request->getModuleName();
+        $controller = $request->getControllerName();
+        $action = $request->getActionName();
+        
+        switch ($module) {
+            case 'cms':
+                if ($controller === 'index') {
+                    return 'homepage';
+                } else {
+                    return Mage::getSingleton('cms/page')->getIdentifier();
+                }
+                break;
+            case 'catalog':
+                return $controller;
+                break;
+            case 'checkout':
+                if ( $action == 'success' ) {
+                    return 'order_success';
+                } else if ( $controller == 'cart' ) {
+                    return 'cart';
+                } else {
+                    return 'checkout';
+                }
+                break;
+            case 'catalogsearch':
+                return "search";
+                break;
+            default:
+                return "$module_$controller";
+            break;
+        }
     }
 }
