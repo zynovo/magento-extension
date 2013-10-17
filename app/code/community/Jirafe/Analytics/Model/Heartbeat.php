e1d1c0e3b50dc1688ee83005bedf846958e964f7
Richard Loerzel <rloerzel@lyonscg.com>
2013-10-17 13:31:09 -0500

    placeholder for cron heartbeat

---
+++ app/code/community/Jirafe/Analytics/Model/Heartbeat.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Heartbeat Model
+ * 
+ * @category  Jirafe
+ * @package   Jirafe_Analytics
+ * @copyright Copyright (c) 2013 Jirafe, Inc. (http://jirafe.com/)
+ * @author    Richard Loerzel (rloerzel@lyonscg.com)
+ * 
+ */
+
+class Jirafe_Analytics_Model_Heartbeat extends Jirafe_Analytics_Model_Api
+{
+    /**
+     * Send heartbeat to Jirafe via cron
+     * 
+     * @return void
+     * @throws Exception if unable to send heartbeat
+     */
+    public function send() 
+    {
+        try {
+            Mage::log('send heartbeat',null,'jirafe_heartbeat.log');
+        } catch (Exception $e) {
+            Mage::throwException('HEARTBEAT ERROR: Jirafe_Analytics_Model_Heartbeat::send(): ' . $e->getMessage());
+        }
+    }
+}
