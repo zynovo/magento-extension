61cd080effe0c6a630311985d8c3053cd458bb34
Richard Loerzel <rloerzel@lyonscg.com>
2013-11-08 10:24:09 -0600

    removed old test scripts

--- process_queue.php
+++
@@ -1,5 +0,0 @@
-<?php 
-require 'app/Mage.php';
-$app = Mage::app('default');
-
-$response = Mage::getModel('jirafe_analytics/batch')->process();
--- test_heartbeat.php
+++
@@ -1,6 +0,0 @@
-<?php 
-require 'app/Mage.php';
-$app = Mage::app('default');
-
-$response = Mage::getModel('jirafe_analytics/curl')->heartbeat();
-Zend_Debug::dump( $response );
