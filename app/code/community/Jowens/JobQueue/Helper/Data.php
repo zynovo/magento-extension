cd324b7ce8bace79201e58483850dc09e53589fb
naqeeb <naqeeb.memon@gmail.com>
2013-12-17 16:17:48 -0500

    Adding Magento JobQueue extension to project

---
+++ app/code/community/Jowens/JobQueue/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+
+class Jowens_JobQueue_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
---
+++ app/code/community/Jowens/JobQueue/Model/Job/Abstract.php
@@ -0,0 +1,53 @@
+<?php
+
+abstract class Jowens_JobQueue_Model_Job_Abstract extends Mage_Core_Model_Abstract
+{
+	private $name;
+	private $storeId;
+
+	public function __construct($name=null) {
+		$this->name = $name ? $name : $this->getType();
+		
+		$this->setStoreId(Mage::app()->getStore()->getStoreId());
+	}
+
+	public abstract function perform();
+
+    public function enqueue($queue="default", $run_at=null) {
+    	$job = Mage::getModel('jobqueue/job');
+    	$job->setStoreId($this->getStoreId());
+    	$job->setName($this->getName());
+    	$job->setHandler(serialize($this));
+    	$job->setQueue($queue);
+    	$job->setRunAt($run_at);
+    	$job->setCreatedAt(now());
+    	$job->save();
+    }
+
+	public function setName($name) 
+	{
+		$this->name = $name;
+		return $this;
+	}
+
+	public function getName() 
+	{
+		return $this->name;
+	}
+
+	public function setStoreId($storeId) 
+	{
+		$this->storeId = $storeId;
+		return $this;
+	}
+
+	public function getStoreId() 
+	{
+		return $this->storeId;
+	}	
+
+	public function getType() 
+	{
+		return get_class($this);
+	}
+}
