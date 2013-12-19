c56a66161b5a3e4b04986ae795ed7f7c614e5233
naqeeb <naqeeb.memon@gmail.com>
2013-12-18 19:26:53 -0500

    Roll back the change to only add a check is_array before the array_merge.

--- app/code/community/Jirafe/Analytics/Model/Data/Attempt.php
+++ app/code/community/Jirafe/Analytics/Model/Data/Attempt.php
@@ -67,15 +67,10 @@ class Jirafe_Analytics_Model_Data_Attempt extends Jirafe_Analytics_Model_Abstrac
                       */
                      if(is_array($response[$pos]))
                      {
-                         $data = array_merge( $data,$response[$pos] );
+                        $data = array_merge( $data,$response[$pos] );
                      }
 
-                     $success = false;
-                     if ($attempt['http_code'] == 200)
-                     {
-                        $success = true;
-                     }
-
+                     $data['success'] = isset($data['success']) ? $data['success'] : false;
                      $data['created_dt'] = $attempt['created_dt'];
 
                      /**
@@ -91,14 +86,14 @@ class Jirafe_Analytics_Model_Data_Attempt extends Jirafe_Analytics_Model_Abstrac
                       */
                      $element = Mage::getModel('jirafe_analytics/data')->load( $data['data_id'] );
                      $element->setAttemptCount( intval( $element->getAttemptCount() ) + 1);
-                     $element->setSuccess( $success ? 1 : 0 );
-                     $element->setCompletedDt(  $success  ? $data['created_dt'] : null);
+                     $element->setSuccess( $data['success'] ? 1 : 0 );
+                     $element->setCompletedDt( $data['success'] ? $data['created_dt'] : null);
                      $element->save();
 
                      /**
                       * If API failure, create error record
                       */
-                     if ( ! $success  ) {
+                     if ( !$data['success'] ) {
                          $data['errors'] = isset($data['errors']) ? json_encode( $data['errors'] ) : null;
                          $data['error_type'] = isset($data['error_type']) ? $data['error_type'] : null;
                          Mage::getModel('jirafe_analytics/data_error')->add( $data, $obj->getId() );
