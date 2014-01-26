2dae326909bf5425c252bfe0834247491ff359a5
John Connor <john.theman.connor@gmail.com>
2014-01-26 16:39:46 -0500

    Refactored Attempt Logging.
    There was an issue when the EventAPI responded to a request with an html
    response, and not a json response (as is currently the case w/ 500s).
    I decided to refactor the code to make it easier to extend and debug
    in the future.

--- app/code/community/Jirafe/Analytics/Model/Data/Attempt.php
+++ app/code/community/Jirafe/Analytics/Model/Data/Attempt.php
@@ -10,16 +10,95 @@
  * @copyright Copyright (c) 2013 Jirafe, Inc. (http://jirafe.com/)
  * @author    Richard Loerzel (rloerzel@lyonscg.com)
  */
-
 class Jirafe_Analytics_Model_Data_Attempt extends Jirafe_Analytics_Model_Abstract
 {
-    /**
-     * Class construction & resource initialization
-     */
-
     protected function _construct()
     {
         $this->_init('jirafe_analytics/data_attempt');
+    }
+
+    protected function _getResponse($response)
+    {
+        $response = array();
+        foreach(json_decode($response, true) as $value) {
+            $response = array_merge($response, $value);
+        }
+        return $response;
+    }
+
+    protected function _createErrorRecord($data)
+    {
+        $data['errors'] = isset($data['errors']) ? json_encode($data['errors']) : null;
+        $data['error_type'] = isset($data['error_type']) ? $data['error_type'] : null;
+        $error = Mage::getModel('jirafe_analytics/data_error');
+        return $error;
+    }
+
+    protected function _createAttemptRecord($created, $id)
+    {
+        $attempt = new $this;
+        $attempt->setDataId($id);
+        $attempt->setCreatedDt($created);
+        $attempt->save();
+        return $attempt;
+    }
+
+    /*
+     * Update data record with success or failure.
+     */
+    protected function _updateDataRecord($id, $success, $created)
+    {
+        $element = Mage::getModel('jirafe_analytics/data')->load($id);
+        $element->setAttemptCount(intval($element->getAttemptCount()) + 1);
+        $element->setSuccess($success ? 1 : 0);
+        $element->setCompletedDt($success ? $created : null);
+        $element->save();
+    }
+
+    /*
+     * Updates data record with failures.
+     *
+     * Used when a response cannot be decoded.
+     */
+    protected function _processError($created, $batch)
+    {
+        foreach ($batch as $_ => $data) {
+            if (!array_key_exists('data_id', $data)) {
+                Mage::helper('jirafe_analytics')->log('ERROR', __METHOD__, 'Batch has no data_id: skiping.');
+                continue;
+            }
+            
+            $id = $data['data_id'];
+            $attempt = $this->_createAttemptRecord($created, $id);
+            $this->_updateDataRecord($id, false, $created)
+        }
+        return true;
+    }
+
+    protected function _processSuccess($created, $batch, $response)
+    {
+        foreach ($batch as $pos => $data) {
+            // Append response and attempt to data object
+            if(is_array($response[$pos])) {
+                $data = array_merge($data, $response[$pos]);
+            }
+            
+            if (!array_key_exists('data_id', $data)) {
+                Mage::helper('jirafe_analytics')->log('ERROR', __METHOD__, 'Batch has no data_id: skiping.');
+                continue;
+            }
+            
+            $id = $data['data_id'];
+            $success = isset($data['success']) ? $data['success'] : false;
+            $attempt = $this->_createAttemptRecord($created, $data);
+            $this->_updateDataRecord($id, $success, $created)
+
+            if (!$success) {
+                $error = $this->_createErrorRecord($data);
+                $error->add($data, $attempt->getId());
+            }
+        }
+        return true;
     }
 
     /**
@@ -29,87 +108,35 @@ class Jirafe_Analytics_Model_Data_Attempt extends Jirafe_Analytics_Model_Abstrac
      * @return boolean
      * @throws Exception if unable to save attempt to db
      */
-    public function add( $attempt = null )
+    public function add($attempt=null)
     {
-
         try {
-
-            if ( $attempt ) {
-
-                 /**
-                  * Get data ids associated with batch
-                  */
-                 $batch = Mage::getModel('jirafe_analytics/batch_data')
-                     ->getCollection()
-                     ->addFieldToFilter('batch_id',array('eq',$attempt['batch_id']))
-                     ->load()
-                     ->getData();
-
-
-                 /**
-                  * Separate API responses in order of batch items
-                  */
-                 $response = array();
-
-                 foreach( json_decode($attempt['response'],true) as $key => $value) {
-                     $response = array_merge($response,$value);
-                 }
-
-                 /**
-                  *  Counter var to tie order of data in response json to batch_order
-                  */
-                 $pos = 0;
-
-                 foreach ($batch as $data) {
-
-                     /**
-                      * Append response and attempt to data object
-                      */
-                     if(is_array($response[$pos]))
-                     {
-                        $data = array_merge( $data,$response[$pos] );
-                     }
-
-                     $data['success'] = isset($data['success']) ? $data['success'] : false;
-                     $data['created_dt'] = $attempt['created_dt'];
-
-                     /**
-                      *  Create record of attempt
-                      */
-                     $obj = new $this;
-                     $obj->setDataId( $data['data_id'] );
-                     $obj->setCreatedDt( $data['created_dt'] );
-                     $obj->save();
-
-                     /**
-                      *   Update data element with success or failure
-                      */
-                     $element = Mage::getModel('jirafe_analytics/data')->load( $data['data_id'] );
-                     $element->setAttemptCount( intval( $element->getAttemptCount() ) + 1);
-                     $element->setSuccess( $data['success'] ? 1 : 0 );
-                     $element->setCompletedDt( $data['success'] ? $data['created_dt'] : null);
-                     $element->save();
-
-                     /**
-                      * If API failure, create error record
-                      */
-                     if ( !$data['success'] ) {
-                         $data['errors'] = isset($data['errors']) ? json_encode( $data['errors'] ) : null;
-                         $data['error_type'] = isset($data['error_type']) ? $data['error_type'] : null;
-                         Mage::getModel('jirafe_analytics/data_error')->add( $data, $obj->getId() );
-                     }
-
-                     $pos = $pos +1;
-                 }
-
-                return true;
-            } else {
-                Mage::helper('jirafe_analytics')->log( 'ERROR', 'Jirafe_Analytics_Model_Data_Attempt::add()' , 'Empty attempt record.');
+            if (!$attempt) {
+                Mage::helper('jirafe_analytics')->log('ERROR', __METHOD__, 'Empty attempt record: aborting.');
+                return false;
+            } if (!array_key_exists('batch_id', $attempt)) {
+                Mage::helper('jirafe_analytics')->log('ERROR', __METHOD__, 'No batch id in attempt record: aborting.');
                 return false;
             }
+
+            $batch = Mage::getModel('jirafe_analytics/batch_data')->load($data['batch_id']);
+            $created = isset($attempt['created_dt']) ? $attempt['created_dt']: '';
+
+            if (!array_key_exists('response', $attempt)) {
+                Mage::helper('jirafe_analytics')->log('ERROR', __METHOD__, 'No response: assuming 500.');
+                return $this->_processError($batch);
+            } else if (!is_array($attempt['response'])) {
+                Mage::helper('jirafe_analytics')->log('ERROR', __METHOD__, 'Invalid response: assuming 500.');
+                return $this->_processError($batch);
+            }
+
+            $response = $this->_getResponse($attempt['response']);
+
+            return $this->_processSuccess($batch, $response);
         } catch (Exception $e) {
             Mage::logException($e);
             return false;
         }
     }
 }
+
